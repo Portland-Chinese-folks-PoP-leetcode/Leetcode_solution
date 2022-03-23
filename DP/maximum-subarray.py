e5e33270663ed20af5a084b9b0a596441962b8f7
Zheng Yune <13390592073@163.com>
2022-03-23 16:53:13 -0400

    update maximun-subarray

--- DP/maximum-subarray.py
+++ DP/maximum-subarray.py
@@ -2,14 +2,19 @@ def maxSubArray(nums):
     """
     :type nums: List[int]
     :rtype: int
+    状态是什么？
+    dp数组的定义：这个dp数组，每个元素的定义是，到在最大的max subarray
     """
+
     dp = [-100000000]*(len(nums))
 
     dp[0] = nums[0]
 
     for i in range(1, len(dp)):
-
-        dp[i] = max(nums[i], dp[i-1]+nums[i])
+        if nums[i] < dp[i-1]+nums[i]:
+            dp[i] = dp[i-1]+nums[i]
+        else:
+            dp[i] = nums[i]
 
     return max(dp)
 
@@ -17,3 +22,8 @@ def maxSubArray(nums):
 
 # 这个解释解释的很好
 #
+
+
+nums = [5, 15, -30, 10, -5, 40, 10]
+print(maxSubArray(nums))
+
