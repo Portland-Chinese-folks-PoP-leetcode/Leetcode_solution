1f554f3e52d7fdbf19d70f6a955a380dd3b843be
Zheng Yune <13390592073@163.com>
2022-04-05 11:13:18 -0400

    update

--- Binary_tree/construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ Binary_tree/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -22,3 +22,30 @@ class Solution(object):
                                preend, postorder, index+1, postend-1)
             return root
         return build(preorder, 0, len(preorder)-1, postorder, 0, len(postorder)-1)
+
+
+
+
+####myun
+class my_solution(object):
+    def constructFromPrePost(self, preorder, postorder):
+        """
+        :type preorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        def build (preorder,prestart,preend,postorder,poststart,postend):
+            if prestart>preend: return None
+            if prestart==preend:return TreeNode(preorder[prestart])
+            root_val=preorder[prestart]
+            left_postend=preorder[prestart+1]
+            print(left_postend)
+            right_prestart=postorder[postend-1]
+            print(right_prestart)
+            left_postend_index=postorder.index(left_postend)
+            right_prestart_index=preorder.index(right_prestart)
+            root=TreeNode(root_val)
+            root.left=build(preorder,prestart+1,right_prestart_index-1,postorder,poststart,left_postend_index)
+            root.right=build(preorder,right_prestart_index,preend,postorder,left_postend_index+1,postend-1)
+            return root
+        return build (preorder,0,len(preorder)-1,postorder,0,len(postorder)-1)
