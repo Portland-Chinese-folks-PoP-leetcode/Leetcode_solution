3f288ae2a5f1e46ade92b649098a497a5d19abfc
Zheng Yune <13390592073@163.com>
2022-04-22 11:32:28 -0400

    update

---
+++ backtracking/subsets.py
@@ -0,0 +1,17 @@
+class Solution:
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(nums, start):
+            # print('track is ',self.track)
+            # print('res is ',self.res)
+            res.append(list(track))
+            for i in range(start, len(nums)):
+                track.append(nums[i])
+                backtrack(nums, i+1)
+                track.pop(-1)
+        res = []
+        track = []
+        backtrack(nums, 0)
+        return res
+
+#https://leetcode-cn.com/problems/subsets/
