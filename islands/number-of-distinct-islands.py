c9557843160295f54e51fdf6b3dc78579b352cbd
Zheng Yune <13390592073@163.com>
2022-04-24 16:23:43 -0400

    为什么这个题目 用list存储path 可以 用str却不行

---
+++ islands/number-of-distinct-islands.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDistinctIslands(self, grid) -> int:
+
+        def island_dfs(grid, i, j, sb, dir):
+            m = len(grid)
+            n = len(grid[0])
+            if i < 0 or j < 0 or i >= m or j >= n:
+                return
+            if grid[i][j] == 0:
+                return
+            grid[i][j] = 0
+            sb.append(str(dir))
+            island_dfs(grid, i-1, j, sb, 1)
+            island_dfs(grid, i+1, j, sb, 2)
+            island_dfs(grid, i, j-1, sb, 3)
+            island_dfs(grid, i, j+1, sb, 4)
+            sb.append(str('-'+str(dir)))
+
+        m = len(grid)
+        n = len(grid[0])
+        island = {}
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    sb = []
+                    island_dfs(grid, i, j, sb, '1')
+
+                    island[str(sb)] = str(sb)
+
+        return len(island)
