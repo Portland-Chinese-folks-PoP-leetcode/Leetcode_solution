11ed9bbc5ad41c1e47c441a68097819f295a5cce
Zheng Yune <13390592073@163.com>
2022-04-23 23:06:01 -0400

    别tm老犯点jb低级错误, == 和=搞清楚, n 和m分别代表的什么的赋值也整明白

---
+++ islands/max-area-of-island.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxAreaOfIsland(self, grid):
+        def island_dfs(grid, i, j):
+            m = len(grid)
+            n = len(grid[0])
+            if i < 0 or j < 0 or i >= m or j >= n:
+                return 0
+            if grid[i][j] == 0:
+                return 0
+
+            grid[i][j] = 0
+            return island_dfs(grid, i+1, j) + island_dfs(grid, i, j+1) + island_dfs(grid, i-1, j) + island_dfs(grid, i, j-1) + 1
+        m = len(grid)
+        n = len(grid[0])
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                print('current i is', i)
+                print('current j is ', j)
+                if grid[i][j] == 1:
+                    print('grid ', i, j, 'is ', 1)
+                    res = max(res, island_dfs(grid, i, j))
+
+                    print(res)
+        return res
+
+
+a = Solution()
+grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [
+    0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
+a.maxAreaOfIsland(grid)
