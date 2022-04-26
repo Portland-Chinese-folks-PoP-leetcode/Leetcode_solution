8ec18137fba88fd833983b63389875f8851d50e3
Zheng Yune <13390592073@163.com>
2022-04-25 21:08:28 -0400

    generate parentheses

---
+++ backtracking/generate-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        track = []
+        res = []
+        left = 0
+        right = 0
+
+        def backtrack(left, right):
+            if left < right:
+                return
+            if left > n or right > n:
+                return
+            if left == n and right == n:
+                track_string = ''.join(map(str, track))
+                res.append(track_string)
+            track.append('(')
+            backtrack(left+1, right)
+            track.pop(-1)
+
+            track.append(')')
+            backtrack(left, right+1)
+            track.pop(-1)
+        backtrack(left, right)
+        return res
