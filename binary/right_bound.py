58a5249f79fb52b6311dfa5695d94ea8bb0f8054
Zheng Yune <13390592073@163.com>
2022-05-06 23:39:55 -0400

    update

--- binary/right_bound.py
+++ binary/right_bound.py
@@ -1,28 +1,30 @@
-def leftbound(nums, target):
+def rightbound(nums, target):
     left = 0
     right = len(nums)
     while left < right:
         mid = left+(right-left)//2
         if nums[mid] == target:
-            right = mid
+            left = mid+1
         elif nums[mid] < target:
             left = mid+1
         elif nums[mid] > target:
             right = mid
-    return left
+    if left == 0:
+        return -1
+    return (left-1) if nums[left-1] == target else -1
 
 
-def leftbound(nums, target):
+def rightbound_v2(nums, target):
     left = 0
     right = len(nums)-1
     while left <= right:
         mid = left+(right-left)//2
         if nums[mid] == target:
-            right = mid-1
-        elif nums[mid] > target:
-            right = mid-1
+            left = mid+1
         elif nums[mid] < target:
             left = mid+1
-    if left >= len(nums) or nums[left] != target:
+        elif nums[mid] > target:
+            right = mid-1
+    if right < 0 or nums[right] != target:
         return -1
-    return left
+    return right
