f5f746598821d9aedb55cf2dbc7dfbd862aad2d0
Zheng Yune <13390592073@163.com>
2022-05-09 15:35:13 -0400

    update

--- linked_list/merge-two-sorted-lists.py
+++ linked_list/merge-two-sorted-lists.py
@@ -3,35 +3,65 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution(object):
-    def mergeTwoLists(self, list1:ListNode, list2:ListNode):
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode):
         """
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
         prehead = ListNode(-1)
-        prev=prehead
+        prev = prehead
         while list1 and list2:
-            if list1.val <=list2.val:
-                prev.next=list1
-                list1= list1.next
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
             else:
-                prev.next=list2
-                list2=list2.next
-            prev=prev.next
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
         prev.next = list1 if list1 is not None else list2
         return prehead.next
 
+# 第二次做 这道题
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        p = dummy
+        p1 = list1
+        p2 = list2
+        while p1 is not None and p2 is not None:
+            if p1.val > p2.val:
+                p.next = p2
+                p2 = p2.next
+            else:
+                p.next = p1
+                p1 = p1.next
+            p = p.next
+        if p1 is not None:
+            p.next = p1
+        if p2 is not None:
+            p.next = p2
+        return dummy.next
+
+
 if __name__ == '__main__':
     # l1 = [1,2,4]
-    l1=ListNode(1)
-    l1.next=ListNode(2)
-    l1.next.next=ListNode(4)
+    l1 = ListNode(1)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(4)
     # l2 = [1,3,4]
-    l2=ListNode(1)
-    l2.next=ListNode(3)
-    l2.next.next=ListNode(4)
-    a=Solution()
-    a.mergeTwoLists(l1,l2)
-    
+    l2 = ListNode(1)
+    l2.next = ListNode(3)
+    l2.next.next = ListNode(4)
+    a = Solution()
+    a.mergeTwoLists(l1, l2)
