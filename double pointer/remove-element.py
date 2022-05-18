3a3005a154530ba3a81c753408d02384d46f714b
Zheng Yune <13390592073@163.com>
2022-05-18 11:39:07 -0400

    update

--- double pointer/remove-element.py
+++ double pointer/remove-element.py
@@ -1,22 +1,22 @@
 # https://leetcode.cn/problems/remove-element/submissions/
 
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums, val: int) -> int:
         if len(nums) == 0:
             return 0
         slow = 0
         fast = 0
-        print(dir(nums))
         while fast < len(nums):
             if nums[fast] != val:
                 nums[slow] = nums[fast]
                 slow += 1
             fast += 1
+        print(nums)
         return slow
 
-nums=[0,1,4,0,2]
-val=0
-solution=Solution()
+
+nums = [0, 1, 4, 0, 2]
+val = 0
+solution = Solution()
 
 print(solution.removeElement(nums, val))
-
