dc544f461f6f1cbe593b858c935a55b3187e0fa1
Zheng Yune <13390592073@163.com>
2022-06-04 21:21:51 -0400

    update

--- differenceArray/range-addition.py
+++ differenceArray/range-addition.py
@@ -1,7 +1,8 @@
-#https://leetcode.cn/problems/range-addition/submissions/
+# https://leetcode.cn/problems/range-addition/submissions/
 class difference:
     # 根据输入的nums 数组 构造出difference array (Construct a difference array based on the input nums array)
     # diff[i]的所记录的定义是 nums[i]比nums[i-1]多了几，少了几。所以说在diff[i]+=val 相当于给nums[i:]后的所有元素都加上了val
+    # 输入一个初始数组，区间操作将在这个数组上进行,构造他的difference array
     def __init__(self, nums: List[int]):
         if len(nums) > 0:
             self.diff = [0 for _ in range(len(nums))]
@@ -9,12 +10,12 @@ class difference:
             for i in range(1, len(nums)):
                 self.diff[i] = nums[i]-nums[i-1]
 
-    def increment(self, i, j, val):
+    def increment(self, i, j, val):  # 给闭区间 [i, j] 增加 val（可以是负数），也就是改变 diff 数组
         self.diff[i] += val
         if j+1 < len(self. diff):
             self.diff[j+1] -= val
 
-    def result(self):
+    def result(self):  # 返回结果数组 ,构造出经过 increment操作后的nums数组
         res = [0 for i in range(len(self.diff))]
         res[0] = self.diff[0]
         for i in range(1, len(self.diff)):
