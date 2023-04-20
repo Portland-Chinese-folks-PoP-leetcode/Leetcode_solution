9658accfbfd95396a7808f8dc6fec52e2e0bbf74
Zheng Yune <yunethecoder@gmail.com>
2023-04-20 13:29:36 -0500

    update

--- Binary_tree/二叉树_纲领/maximumDepthofBinaryTree.java
+++ Binary_tree/二叉树_纲领/maximumDepthofBinaryTree.java
@@ -1,18 +1,5 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
+
+//Definition for a binary tree node.
 class TreeNode {
     int val;
     TreeNode left;
